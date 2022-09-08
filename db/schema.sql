91257bb849a9d275fa5170a826b2f27eeed670f5
Simone Brin <simonebrin@gmail.com>
2022-09-08 12:24:43 -0400

    fixed 404 issue

--- db/schema.sql
+++ db/schema.sql
@@ -15,3 +15,5 @@ CREATE TABLE candidates (
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
+
+source db/seeds.sql;
--- server.js
+++ server.js
@@ -119,11 +119,67 @@ app.get("/api/candidates", (req, res) => {
   });
 });
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+
+
+app.get('/api/parties', (req, res) => {
+    console.log("route started")
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        console.log(rows)
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+  app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        // checks if anything was deleted
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Party not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+
+  // Default response for any other request (Not Found)
+  app.use((req, res) => {
+    res.status(404).end();
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
