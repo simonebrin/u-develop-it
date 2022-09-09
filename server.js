1e973c529ec2cd1490fb50f871490f76069c2bac
Simone Brin <simonebrin@gmail.com>
2022-09-09 09:07:11 -0400

    added route to change candidate's party

--- server.js
+++ server.js
@@ -99,6 +99,35 @@ app.post("/api/candidate", ({ body }, res) => {
   });
 });
 
+// Update a candidate's party
+app.put("/api/candidate/:id", (req, res) => {
+  const errors = inputCheck(req.body, "party_id");
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Candidate not found",
+      });
+    } else {
+      res.json({
+        message: "success",
+        data: req.body,
+        changes: result.affectedRows,
+      });
+    }
+  });
+});
+
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
   const sql = `SELECT candidates.*, parties.name 
@@ -119,67 +148,63 @@ app.get("/api/candidates", (req, res) => {
   });
 });
 
+app.get("/api/parties", (req, res) => {
+  console.log("route started");
+  const sql = `SELECT * FROM parties`;
+  db.query(sql, (err, rows) => {
+    console.log(rows);
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
 
+app.get("/api/party/:id", (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
 
-
-
-app.get('/api/parties', (req, res) => {
-    console.log("route started")
-    const sql = `SELECT * FROM parties`;
-    db.query(sql, (err, rows) => {
-        console.log(rows)
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
+app.delete("/api/party/:id", (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
       res.json({
-        message: 'success',
-        data: rows
+        message: "Party not found",
       });
-    });
-  });
-
-  app.get('/api/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
+    } else {
       res.json({
-        message: 'success',
-        data: row
+        message: "deleted",
+        changes: result.affectedRows,
+        id: req.params.id,
       });
-    });
+    }
   });
+});
 
-  app.delete('/api/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: res.message });
-        // checks if anything was deleted
-      } else if (!result.affectedRows) {
-        res.json({
-          message: 'Party not found'
-        });
-      } else {
-        res.json({
-          message: 'deleted',
-          changes: result.affectedRows,
-          id: req.params.id
-        });
-      }
-    });
-  });
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
 
-  // Default response for any other request (Not Found)
-  app.use((req, res) => {
-    res.status(404).end();
-  });
-
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
