13d3463eff94e75131b8317e813b7521f3ca1c0f
Simone Brin <simonebrin@gmail.com>
2022-09-10 15:07:14 -0400

    modularized database connection code

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2");
+const db = require('./db/connection');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -10,18 +10,7 @@ const inputCheck = require("./utils/inputCheck");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    // Your MySQL username,
-    user: "root",
-    // Your MySQL password
-    password: "password",
-    database: "election",
-  },
-  console.log("Connected to the election database.")
-);
+
 
 //   db.query(`SELECT * FROM candidates`, (err, rows) => {
 //     console.log(rows);
